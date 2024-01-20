980da5afa93ab852f945e395faf1bffbec70a82a
student <student@muthayammal.local>
2024-01-20 16:22:20 +0530

    swl

---
+++ script.js
@@ -0,0 +1,254 @@
+// alert("Welcome to learn JavaScript");
+// let myName = prompt("Enter your name ?");
+// consolkkllkj;ljkpj[uiojlm e.log("Welcome", myName);
+// let sum = prompt("Enter a number");
+// console.log("Your Answer Is", sum + 45);
+// let a = 89; //numbers
+// let v = 7;
+// console.log(a * v);
+// let Score = 0;
+// Score = 45;
+// console.log("Final Score" + " : " + Score);
+// console.log(45);
+// console.log("Gokul");
+// let arr = [6, 7, 4, 5, 45]; //array
+// console.log(arr);
+// let error = "syntax Error"; //strings
+// console.error(error);
+// console.table({
+//   First_name: "Gokul",
+//   Last_name: "J",
+// });
+// console.warn("Welcome Gokul");
+// //object
+// let object = {
+//   First_nahmrj: "Gokul",
+//   Last_name: "J",
+//   Age: 45,
+// };
+// console.log(object);
+// let lar = document.getElementBy("ik");
+// textContent = lar;
+// document.getElementById("ik").textContent="Hello World✌️✌️✌️"
+// //addition
+// let f = 67;
+// let t =h 56;
+// let r = f + t;
+// console.log(r);
+// var tt = 66;
+// let totalAns = console.log(r + tt);
+// // console.log(123+66)
+// console.log(error + "-->" + r);
+//console.log(errror)
+// const u = 645;
+// u = 1;
+// console.log(u); //const are not able to re-defind
+// {
+//   let u = 45;
+//   u = 67;
+// }
+// // console.log(u)
+// let u = 30;
+// console.log(u);
+// u = 78;
+// console.log(u);
+// console.log(u);
+// {
+//   var po = 25;
+//   po = 09;
+// }
+// console.log(po);
+// var xx = 39; //var are able to re-define
+// let yy = 44; //let are not able to re-define
+// let zzz = xx + yy;
+// console.log(zz);
+// xx = 44;
+// yy = 323;
+// console.log(`${xx}  ${yy}`);
+// var yy = 89;
+// console.log(yy);
+// var xxz = 82;
+// console.log(xx);
+// let G = 10;
+// {
+//   let G = 13;
+//   consv
+// console.log(G);
+// const aa = 12;
+// const obb = 91;
+// {
+//   const aa = 2;
+//   let aa = 1;
+//   console.log(aa+aa)
+// }
+// const ss = 5;
+// console.log(ss);
+
+/*🤖🤖🤖🤖🤖🤖🤖*/
+
+// let myName = "GOKUL";
+// console.log(typeof myName);
+// a = 54.60867;
+// b = "ghfgh";
+// c = 6;
+// console.log(a + b);
+// console.log(a - b + 5);
+// console.log(a * b - 6);
+// console.log((a % b) + 8);
+// console.log((a / b) * 6);
+// console.log(typeof a);
+// let myString = a + b;
+// console.log("The output" + "--->" + myString);
+// console.log(typeof myString);
+// console.log(a == b);
+// console.log(fNumber.isInteger(b));
+// console.log(isNaN(b));
+// console.log(Number.isNaN(a));
+// console.log(parseFloat(a).toFixed(1));
+// console.log("This Number"+parseFloat(a))
+// console.log(Number.isNaN(b));
+// console.log(Number.isInteger(b));
+// console.log(typeof b);
+// console.log(Math.PI);
+// console.log(Math.round(Math.PI));
+// console.log(Math.ceil(6.508));
+// console.log(Math.floor(6.908));
+// console.log(Math.pow(2,19))
+// console.log(Math.max(56, 67, 347, 897, 907, 233456));
+// console.log(Math.min(56, 67, 347, 897, 907, 233456));
+// console.log(Math.random());
+// console.log(Math.random() * 5);
+// console.log(Math.round(Math.random() * 16) + 2);
+// let randomNumber = Math.round(Math.random() * 16) + 2;
+// console.log(randomNumber);
+// console.log(randomNumber - 1);
+
+/*☠️☠️☠️☠️☠️☠️☠️☠️☠️*/
+// let d =
+//   "48230576889-232323232394875fhjiyngfuixxdvmkdjdicu[wzaeiq20302-48983875555555555555555bvuh";
+// let tf = true;
+// let a = "GOKUL";
+// let replay;
+// console.log(a.charAt(Math.floor(Math.random()*5)));//to print a random char in a string "using charAt()"
+// console.log(a.length);
+// console.log(a.charAt(Math.floor(Math.random() * a.length)));
+// console.log(d.length);
+// console.log(a.charAt(Math.floor(Math.random()*d.length)));
+
+// if (tf ==true) {
+//   replay = `Who is this`;
+// } else if (tf == false && a == "GOKUL") {
+//   replay = `Hello ${a}`;
+// } else if (tf && a == "GOKU") {
+//   replay = `This Is Not You  `;
+// } else {
+//   replay = `Who Are You ?🤷‍♂️🤷‍♂️`;
+// }
+// console.log(replay);
+// let m = "90";
+// let m = "10";
+// switch (m) {
+//   case "90":
+//     console.log("A+");
+//     break;
+//   case "80":
+//     console.log("A");
+//     break;
+//   case "70":
+//     console.log("B+");
+//     break;
+//   case "60":
+//     console.log("B");
+//     break;
+//   case "50":
+//     console.log("C");
+//     break;
+//   case "40":
+//     console.log("fail");
+//     break;
+//   default:
+//     console.log("Border Pass");
+// }
+
+// let d = "go";
+// let c = d == "go" ? "Hello" : "bye";
+// console.log(c);
+
+// let mark = 49;
+// let A = mark > 90 ? "Your Grade Is O":"Absent";
+// console.log(A);
+
+// let t = 57;
+// let b =
+//   t <= 90
+//     ? "Your Grade Is A"
+//     : t <= 80
+//     ? "Your Grade Is B+"
+//     : t <= 79
+//     ? "Your Grade Is B "
+//     : t <= 69
+//     ? "Your Grade is C"
+//     : t <= 10
+//     ? "Your Grade is Fail"
+//     : "your grade is  Absent";
+
+// console.log(b);
+
+// function myFun(b, c) {
+//   a = b + c;
+//   return a;
+// }
+// console.log(myFun(3850, 350));
+
+// console.log("goukl@587.gmail.com".slice(0, 10));
+// console.log("goukl@587.gmail.com".slice(0, "goukl@gmail.com".indexOf(".com")));
+
+// function myEmail(aa) {
+//   vb = aa.slice(0, aa.indexOf("@gmail.com"));
+//   return vb;
+// }
+// console.log(myEmail("gokulj76@gmail.com"));
+// console.log(myEmail("vasanthakumar2222va@gmail.com"));
+
+// let myArr = [];
+// myArr[0] = "G";
+// myArr[1] = "O";
+// myArr[2] = "K";
+// myArr[3] = "U";
+// myArr[4] = "L";
+// console.log(myArr[0])
+// console.log(myArr[].length())
+// console.log(myArr.length)
+// console.log(myArr.push("j"));
+// console.log(myArr);
+
+// let myObj = {
+//   Full_name: {
+//     First_name: "GOKUL",
+//     Last_name: "J",
+//   },
+//   age: 20,
+//   place: "Salem",
+// };
+// console.log(myObj.Full_name.First_name);
+
+//👻👻👻👻👻👻👻👻👻//
+
+//DOM//
+
+function myEvent() {
+  document.getElementById("dat").textContent = Date();
+}
+
+document.getElementById("exit").addEventListener("click", function () {
+  alert("Your Are Loging Out");
+});
+
+document.getElementById("p").innerHTML = "Hello This is  Goku";
+
+let text ="Greg understood that it situation would make Michael";
+  
+text = text.replaceALL("Greg", "GOKUL");
+text = text.replaceALL("it", "is");
+
+document.getElementById("replace").innerHTML = text;
